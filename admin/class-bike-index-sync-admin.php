9c441d9a9f70ffdb9a2e511d0bf0e3498f61c9e0
Bryan Purcell <purcebr@gmail.com>
2014-06-04 23:17:49 -0500

    adding queue count to admin settings

--- admin/class-bike-index-sync-admin.php
+++ admin/class-bike-index-sync-admin.php
@@ -222,12 +222,9 @@ class Bike_Index_Sync_Admin {
 
 	public function bike_index_settings_manual_update() {
 		$options = get_option('bike-index-sync-settings');
-		if(isset($options['bikeindex_sync_queue']) && $options['bikeindex_sync_queue'] != '' && !empty($options['bikeindex_sync_queue']))
-			$queue_number = sizeof($options['bikeindex_sync_queue']);
-		else
-			$queue_number = "Empty";
+		$queue_number = $options['bikeindex_sync_queue'];
+		var_dump($queue_number);
 		echo '<p>Items in Queue: <strong>' . $queue_number . '</strong></p>';
-		echo '<input type="checkbox" name="bike-index-sync-settings[manual_update]" value="Yes"/>';
 	}
 
 
